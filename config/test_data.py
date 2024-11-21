5d97fdeb2efd24b4a7e73e18df895f142ce8553c
Qualitrix <suneel.barigala@gmail.com>
2024-11-21 12:04:36 +0530

    Code Cleanup withe better commenting

---
+++ config/test_data.py
@@ -0,0 +1,11 @@
+# config/test_data.py
+
+# Global test data
+MOBILE_NUMBER = "7507233095"
+OTP_CODE = "1234"
+
+# List of search keywords for tests
+SEARCH_KEYWORDS = ["Zzzz","Myntra", "Flipkart", "Amazon"]
+
+
+# Locator for Mynthra Search
